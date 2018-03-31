327de184b4bbb15a85c445840d3d339517776a22
Judson Stevens <Judson.Stevens2gmail.com>
2018-03-30 21:10:26 -0600

    Implementation of server class and printer class

--- lib/server.rb
+++ lib/server.rb
@@ -2,7 +2,9 @@ require 'socket'
 require "./lib/printer.rb"
 
 class Server
-
+  attr_reader :server,
+              :printer,
+              :client
 
   def initialize
     @server = TCPServer.new(9292)
@@ -10,14 +12,43 @@ class Server
   end
 
   def start_server
-    client = @server.accept
+    @client = @server.accept
     @printer.ready_message
     loop do
-      while line = client.gets and !line.chomp.empty?
-        request_lines << lines.chomp
+      request_lines = []
+      while line = @client.gets and !line.chomp.empty?
+        request_lines << line.chomp
       end
+      got_a_request(request_lines)
+      parse_request(request_lines)
+      send_hello_world
+      if @printer.hello_world_counter == 3
+        @client.close
+        exit
+      end
+      output_diagnostics
+    end
 
+  end
+
+  def send_hello_world
+    response = @printer.hello_world_response
+    output = @printer.output_formatted(response)
+    header = @printer.headers_formatted(output)
+    @client.puts header
+    @client.puts output
+  end
+
+  def got_a_request(request_lines)
+    @printer.got_a_request_message(request_lines)
+  end
+
+  def output_diagnostics
+    @printer.print_debug
   end
 
 
 end
+
+x = Server.new
+x.start_server
